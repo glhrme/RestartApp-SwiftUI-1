3022a21876b5e77a3a1b9128b4f2518e7ce39f07
Guilherme dos Santos <guilherme@guisantos.com.br>
2022-01-21 23:27:01 -0300

    Initial

---
+++ Restart App/Restart_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_AppApp.swift
+//  Restart App
+//
+//  Created by Guilherme de Assis dos Santos on 21/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Restart_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
